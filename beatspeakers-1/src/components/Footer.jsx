0b375fe870178fc4ad79d1e0d6df97de7aa91f3c
Chris Maylor <chrismaylor1@hotmail.com>
2024-05-17 16:47:40 +0100

    adds info and icon to footer

--- beatspeakers-1/src/components/Footer.jsx
+++ beatspeakers-1/src/components/Footer.jsx
@@ -1,10 +1,12 @@
+import { FaInstagram } from "react-icons/fa";
+
 export const Footer = () => {
     
     return (
                 <div className=" w-screen h-[200px]  bottom-0 z-10 bg-[#121212]  text-lg">
                 
-        
-                    
+                <FaInstagram color="white" size="80px"/>
+                   <p>Check us out on Instagram</p> 
                     
                 
                     

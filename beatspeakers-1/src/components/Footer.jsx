a58470b4389bb77364ad80f724a99148807012bb
Chris Maylor <chrismaylor1@hotmail.com>
2024-05-18 16:44:05 +0100

    adds in footbar nav links

--- beatspeakers-1/src/components/Footer.jsx
+++ beatspeakers-1/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { FaInstagram } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 export const Footer = () => {
     
@@ -7,7 +8,39 @@ export const Footer = () => {
                 
                 <FaInstagram color="white" size="80px"/>
                    <p>Check us out on Instagram</p> 
-                    
+                   <p>Useful links</p>
+                   <ul className="hidden md:flex items-center gap-5">
+                    <li>
+                        <Link to="/" className="text-gray-300 hover:text-[#43ad2e] z-50 pointer-events-auto">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/about" className="text-gray-300 hover:text-[#43ad2e]">
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/workshops" className="text-gray-300 hover:text-[#43ad2e]">
+                            Workshops
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/artists" className="text-gray-300 hover:text-[#43ad2e]">
+                            Artists
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/gallery" className="text-gray-300 hover:text-[#43ad2e]">
+                            Gallery
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/contact" className="text-gray-300 hover:text-[#43ad2e]">
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
                 
                     
                     
